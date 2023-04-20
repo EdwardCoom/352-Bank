0ec66c280e6a87f1cabee72fd23ea53666b8d70c
Zach Coomer <zaccoom@ut.utm.edu>
2023-04-20 13:42:10 -0500

    relocated Debit_With... test method within the UnitTest1 class

--- BankTest/UnitTest1.cs
+++ BankTest/UnitTest1.cs
@@ -11,23 +11,23 @@ namespace BankTest
         public void TestMethod1()
         {
         }
-    }
 
-    // unit test code  
-    [TestMethod]
-    public void Debit_WithValidAmount_UpdatesBalance()
-    {
-        // arrange  
-        double beginningBalance = 11.99;
-        double debitAmount = 4.55;
-        double expected = 7.44;
-        BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+        // unit test code  
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // arrange  
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
 
-        // act  
-        account.Debit(debitAmount);
+            // act  
+            account.Debit(debitAmount);
 
-        // assert  
-        double actual = account.Balance;
-        Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+            // assert  
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
     }
 }
