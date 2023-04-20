e46058ab3aab21223a8f0f60f4083bf8cd670545
Zach Coomer <zaccoom@ut.utm.edu>
2023-04-20 14:08:07 -0500

    added test for Debit_WhenAmount... to test for debit amount being more than the beginningBalance

--- BankTest/UnitTest1.cs
+++ BankTest/UnitTest1.cs
@@ -29,5 +29,38 @@ namespace BankTest
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
+
+
+        //unit test method  
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange  
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act  
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException  
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            double beginningBalance = 20.00;
+            double debitAmount = 200.00;
+            BankAccount account = new BankAccount("Mr. Patrick Star", beginningBalance);
+
+            //act
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException
+        }
+
+
     }
 }
